e2b5024684008f568618eabe968d162ec5e51ca9
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-18 20:07:04 -0300

    FileStream and StreamReader.

--- Stream/Program.cs
+++ Stream/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+namespace Course
+{
+    class Program { 
+    
+    static void Main(string[] args)
+        {
+            // Add the path of your file.
+            string path = @"C:\Users\guguo\source\repos\Stream\Stream\NovaPasta\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
